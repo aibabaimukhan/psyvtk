616b88c596cef5bb8f6e806d3356c4ce5d56cbc6
Rep <159976285+aibabaimukhan@users.noreply.github.com>
2026-02-10 12:30:28 +0500

    Add files via upload

---
+++ app-files/data.js
@@ -0,0 +1,65 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cabinet",
+      "name": "cabinet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "yaw": 0.6468834816692084,
+        "pitch": 0.4589079733403505,
+        "fov": 1.3477036756231675
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.7926761838918992,
+          "pitch": 0.5431068899055767,
+          "title": "Консультативно-диагностическая зона",
+          "text": "<br>"
+        },
+        {
+          "yaw": 1.9198687451962373,
+          "pitch": 0.287890144947367,
+          "title": "Коррекционно-развивающая зона",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.7674830101249874,
+          "pitch": 0.8327958048847002,
+          "title": "Зона снижения эмоционального напряжения",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.22015899197677413,
+          "pitch": 0.6513976087534328,
+          "title": "Групповая тренингово-просветительская зона",
+          "text": "<br>"
+        },
+        {
+          "yaw": -1.1934349104776132,
+          "pitch": 0.2450810131904788,
+          "title": "Информационная зона педагога-психолога",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
